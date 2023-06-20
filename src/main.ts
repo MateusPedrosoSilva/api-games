d54a7c71136d92ade739bcffda4eda9cf4dde9bd
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2023-06-19 22:36:26 -0300

    install and configure swagger and remove hello world route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Informações para Portal do fornecedor')
+    .setDescription('API de consulta de informações do RMS do Grupo Líder')
+    .setVersion(process.env.APP_VERSION)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(process.env.APP_API_ROUTE, app, document);
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
